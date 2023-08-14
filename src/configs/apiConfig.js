765881efbc2a40021e1c42a443ff1d76aafc6ccd
ziaurjoy <ziaurjoy802@gmail.com>
2023-08-14 18:31:45 +0600

    change domain name

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -1,1 +1,2 @@
-export const apiBaseUrl = 'http://org2.localhost:8000' //No Trailing Slash
+// export const apiBaseUrl = 'http://org2.localhost:8000' //No Trailing Slash
+export const apiBaseUrl = 'http://org6.localhost:8000' //No Trailing Slash
